17e33e7251fbafa31586e130c81ff0ec9414ff64
Aron Atkins <aron@gweep.net>
2013-12-03 20:53:16 -0500

    fix js-mode initialization

--- aron-init.el
+++ aron-init.el
@@ -234,8 +234,9 @@
 ;;(require 'tramp)
 ;;(setq tramp-default-method "ssh")
 
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;;(require 'js2-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
