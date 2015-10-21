63064a003f325a2fa9c70335c732cda3e303678a
Aron Atkins <aron@gweep.net>
2015-10-20 21:13:49 -0400

    elim tramp post

--- lisp/aron-init.el
+++ lisp/aron-init.el
@@ -365,9 +365,5 @@
 (autoload 'cmake-font-lock-activate "cmake-font-lock" nil t)
 (add-hook 'cmake-mode-hook 'cmake-font-lock-activate)
 
-;; Vagrant
-(eval-after-load 'tramp
-  '(vagrant-tramp-enable))
-
 (provide 'aron-init)
 ;;; aron-init.el ends here
